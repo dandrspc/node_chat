a46dfc67a9bc7f1b32a08a8f120311e393da717a
Daniel Palacios Carabalí <dapalca@gmail.com>
2020-05-20 22:06:37 -0500

    Request, Header & Custom-headers

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 var app = express()
 const PORT = process.env.PORT || 3000
+app.use(bodyParser.json())
 app.use(router)
 
-router.get('/message', function (req, res) {
+router.get('/message', (req, res) => {
+    console.log(req.headers)
+    res.header({
+        "custom-header": "Our personal value"
+    })
     res.send('Message List')
 })
 
 router.post('/message', function (req, res) {
-    res.send('Message Added')
+    console.log(req.body)   //get body
+    console.log(req.query)  //get URL query arguments
+    res.send(`Message ${req.body.text} Added`)
 })
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log('Server is listening at http://localhost:3000')
 })
