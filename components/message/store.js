156ea56ec552806a51b79aa068070a256ac1bfa9
Daniel Palacios Carabalí <dapalca@gmail.com>
2020-05-23 01:08:19 -0500

    [db] MongoDb: addMessage and getMessages

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,26 @@
+const db = require('mongoose')
+const Model = require('./model')
+
+const MONGO_URI = process.env.MONGO_URI
+db.Promise = global.Promise
+db.connect(MONGO_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log('[db] database connected successfully')
+    })
+    .catch(err => {
+        console.log('[db] database failed to connect ' + err)
+    })
+
 const list = []
 
 function addMessage(message) {
-    list.push(message)
+    const myMessage = new Model(message)
+    myMessage.save()
 }
 
-function getMessages() {
-    return list
+async function getMessages() {
+    const messages = await Model.find()
+    return messages
 }
 
 module.exports = {
